c43d707e735cf7aeb84ad7c7fbd47384d2b290e1
realsnipc <snipc.mail@proton.me>
2024-01-16 23:31:24 +0530

    add express.json and urlencoded

--- api/src/app.js
+++ api/src/app.js
@@ -4,6 +4,9 @@ const app= express()
 
 require("./db/connection") // Connection to MongoDB
 
+app.use(express.json());
+app.use(express.urlencoded());
+
 app.listen(constants.port, ()=>{
     console.log("App listening on http://localhost:"+ constants.port)
 })
